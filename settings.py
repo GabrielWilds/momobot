1cf4e6a402163e37c1ae0a8af74a3fd4c584e9e2
Gabriel Wilds <jetsetlemming@gmail.com>
2011-09-02 04:43:54 -0400

    cleanup

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # The IRC nickname and password to connect and identify with
-NICKNAME = 'momobot_jsl'
+NICKNAME = 'momobot'
 PASSWORD = ''
 
 # The IRC server and port to connect to
@@ -15,9 +15,9 @@ CHANNEL = '#op9'
 COMMAND_INDICATORS = ['!', '.', 'momo, ']
 
 # How long to wait before replying to a command (secs)
-COMMAND_DELAY = 0.3
+COMMAND_DELAY = 0.2
 
 # The IRC VERSION reply
 CTCP_VERSION = 'Momobot v0.1a'
 
-SELFDESTRUCT_USERS = ['Randomskk', 'redd', 'Xaiter', 'jsl']
+SELFDESTRUCT_USERS = ['Randomskk', 'redd', 'Xaiter', 'JSL']
